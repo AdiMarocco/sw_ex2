4c54757175eadfbbc31dcfceae029079efe34f55
AdiMarocco <118695745+AdiMarocco@users.noreply.github.com>
2022-11-30 21:00:32 +0200

    acsadc

--- main.c
+++ main.c
@@ -1,31 +1,31 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include "my_mat.h"
-#define false 0
-#define true 1
+// <<<<<<< HEAD
+// #include <stdio.h>
+// #include "my_mat.h"
+// #define false 0
+// #define true 1
 
-int main(){
-    char input;
-    int matrix[N][N] ;
-    scanf("%d", &input);
-    while(input!='D'){
-      if (input=='A'){
-            First(matrix); 
-            floydWarshall(matrix);
-      }
-      if (input=='B'){
-            Second(matrix);
-      }
-      if (input=='C'){
-        Third(matrix);
-      }
+// int main(){
+//     char input;
+//     int matrix[N][N] ;
+//     scanf("%d", &input);
+//     while(input!='D'){
+//       if (input=='A'){
+//             First(matrix); 
+//             floydWarshall(matrix);
+//       }
+//       if (input=='B'){
+//             Second(matrix);
+//       }
+//       if (input=='C'){
+//         Third(matrix);
+//       }
 
-    scanf("%d", &input);
-    }
+//     scanf("%d", &input);
+//     }
 
 
 
-=======
+// =======
 #include <stdio.h>
 #include "my_mat.h"
 #define false 0
@@ -52,5 +52,5 @@ int main(){
 
 
 
->>>>>>> 87e1b356a7d077a17141b2aa1793ed86b6c30fb8
+
 return 0; }
--- my_mat.h
+++ my_mat.h
@@ -1,23 +1,23 @@
-<<<<<<< HEAD
-#ifndef _MY_MAT_
-#define _MY_MAT_
-#include <stdio.h>
-#define N 10
+// <<<<<<< HEAD
+// #ifndef _MY_MAT_
+// #define _MY_MAT_
+// #include <stdio.h>
+// #define N 10
 
 
 
-//matrix input from user
-void First(int input[N][N]);
+// //matrix input from user
+// void First(int input[N][N]);
 
-//will return if there is path from i to j
-void Second (int input [][]);
+// //will return if there is path from i to j
+// void Second (int input [][]);
 
-//will return the shortes path from i to j
-// if there is no path it will return -1
-void Third (int input [][]);
+// //will return the shortes path from i to j
+// // if there is no path it will return -1
+// void Third (int input [][]);
 
-#endif
-=======
+// #endif
+// =======
 #ifndef _MY_MAT_
 #define _MY_MAT_
 #include <stdio.h>
@@ -37,4 +37,4 @@ void Second (int input[N][N]);
 void Third (int input[N][N]);
 
 #endif
->>>>>>> 87e1b356a7d077a17141b2aa1793ed86b6c30fb8
+
